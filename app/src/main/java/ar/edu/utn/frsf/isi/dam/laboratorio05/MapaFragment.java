f9184e1e7675b6632dc700c4269833f6fbe62562
David Harispe <david_h.94@hotmail.com>
2018-11-17 03:09:09 -0300

    Terminado Req01 de Lab 05. El unico detalle seria que la primera vez que se abre el mapa, pide los permisos para obtener nuestra ubicacion y si aceptamos, recien en la proxima vez que se abra el mapa se va a poder hacer uso de eso. Esto pasa porque requestPermissions() es asincronico... No se me ocurre una solución...

--- app/src/main/java/ar/edu/utn/frsf/isi/dam/laboratorio05/MapaFragment.java
+++ app/src/main/java/ar/edu/utn/frsf/isi/dam/laboratorio05/MapaFragment.java
@@ -1,12 +1,18 @@
 package ar.edu.utn.frsf.isi.dam.laboratorio05;
 
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -18,7 +24,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 public class MapaFragment extends SupportMapFragment implements OnMapReadyCallback {
     private GoogleMap miMapa;
 
-    public MapaFragment() { }
+    public MapaFragment() {
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -34,6 +41,47 @@ public class MapaFragment extends SupportMapFragment implements OnMapReadyCallba
     @Override
     public void onMapReady(GoogleMap map) {
         miMapa = map;
+        actualizar();
+    }
+
+    private void actualizar(){
+        final String[] perm = {Manifest.permission.ACCESS_FINE_LOCATION};
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && (ActivityCompat.checkSelfPermission(getContext(),Manifest.permission.ACCESS_FINE_LOCATION)
+                !=PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getContext(),Manifest.permission.ACCESS_COARSE_LOCATION)
+                !=PackageManager.PERMISSION_GRANTED)){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity()
+                    , Manifest.permission.ACCESS_FINE_LOCATION))
+                (new AlertDialog.Builder(getContext()))
+                        .setTitle(getString(R.string.solicPermisoUbic)).setMessage(getString(R.string.textoSolicPermUbic))
+                        .setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ActivityCompat.requestPermissions(getActivity(), perm, 5);
+                                return;}
+                        })
+                        .setNegativeButton(getString(R.string.no), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(getContext(), getString(R.string.errorUbicacion), Toast.LENGTH_LONG).show();}
+                        }).create().show();
+            else
+                ActivityCompat.requestPermissions(getActivity(),perm,5);
+            return;}
+        miMapa.setMyLocationEnabled(true);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+        switch(requestCode) {
+            case 5:
+                if(grantResults.length>0&& grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    actualizar();
+                else
+                    Toast.makeText(getActivity(), getString(R.string.errorUbicacion), Toast.LENGTH_LONG).show();
+        }
     }
 }
 
